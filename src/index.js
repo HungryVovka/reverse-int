e7ca2c98fbf7d8338f21aa34476349d8205f1dd3
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-11 15:35:39 +0500

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0){
+    return parseInt(String(n)
+                    .split("")
+                    .reverse()
+                    .join(""));
+  } else {
+    return 0 - parseInt(String(n)
+                        .split("")
+                        .reverse()
+                        .join(""));
+  }
 }
