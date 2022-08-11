dcf266ad1d95e17aafb4ad6fd4ef0d693c3ea99f
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-11 15:40:13 +0500

    fix: solution doesn't need negative values. ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 module.exports = function reverse (n) {
-    if (n > 0){
     return parseInt(String(n)
                     .split("")
                     .reverse()
                     .join(""));
-  } else {
-    return 0 - parseInt(String(n)
-                        .split("")
-                        .reverse()
-                        .join(""));
-  }
 }
